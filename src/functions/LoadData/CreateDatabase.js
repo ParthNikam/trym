89696a1675d081c8053a92e8121f3ddd41d637c5
parth108 <108parthnikam@gmail.com>
2023-12-18 21:47:23 +0530

    cookie expiry logic correctION

--- src/functions/LoadData/CreateDatabase.js
+++ src/functions/LoadData/CreateDatabase.js
@@ -12,26 +12,38 @@ export default function CreateDatabase() {
   useEffect(() => {
     const checkAndSaveUserData = async () => {
       const expiryDate = cookies.dataExpiryDate;
-      // console.log(expiryDate);
+
       if (expiryDate) {
         if (isExpired(expiryDate)) {
-          const newExpiryDate = new Date();
-          newExpiryDate.setDate(newExpiryDate.getDate() + 2);
-          setCookie("dataExpiryDate", newExpiryDate, { path: "/" });
-          
-          const userData = await dispatch(fetchAllUsers());
-          console.log("bro got data: ", userData)
-          saveUserDataToIndexedDB(userData);
+          try {
+            const userData = await dispatch(fetchAllUsers());
+            saveUserDataToIndexedDB(userData);
+
+            const newExpiryDate = new Date();
+            newExpiryDate.setHours(newExpiryDate.getHours() + 2);
+            setCookie("dataExpiryDate", newExpiryDate, { path: "/" });
+
+            console.log("Bro, got data and refreshed: ", userData);
+          } catch (error) {
+            console.error("Error fetching or saving user data:", error);
+          }
         } else {
           console.log("User database already exists in IndexedDB");
         }
       } else {
-        // If dataExpiryDate does not exist, set a new expiry date cookie of 2 days
+        // If dataExpiryDate does not exist, set a new expiry date cookie of 2 hours
         const newExpiryDate = new Date();
-        newExpiryDate.setDate(newExpiryDate.getDate() + 2);
+        newExpiryDate.setHours(newExpiryDate.getHours() + 2);
         setCookie("dataExpiryDate", newExpiryDate, { path: "/" });
-        const userData = await dispatch(fetchAllUsers());
-        saveUserDataToIndexedDB(userData);
+
+        try {
+          const userData = await dispatch(fetchAllUsers());
+          saveUserDataToIndexedDB(userData);
+
+          console.log("Bro, got data and set expiry: ", userData);
+        } catch (error) {
+          console.error("Error fetching or saving user data:", error);
+        }
       }
     };
 
@@ -45,7 +57,7 @@ function isExpired(data) {
   const timestamp = data; // Assuming you have a timestamp property
 
   if (timestamp) {
-    const expirationTime = new Date(timestamp + 60 * 60 * 1000); // Expiry of 2 days
+    const expirationTime = new Date(timestamp + 2 * 60 * 60 * 1000);
     return expirationTime < new Date();
   }
 
